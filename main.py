f128739329fef7f352048e765991bd7b95a2e4a2
Artur <artur.gawrych@gmail.com>
2025-06-20 16:34:52 +0100

    add user prompt via cli

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+
+
+if len(sys.argv) < 2:
+    print("Please pass the query as an argument.")
+    exit(1)
+else:
+    user_prompt = sys.argv[1]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages
+    )
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
+
 
 print(response.text)
 
