780dcd95f29611fdd7af96cd4a0d9c49a3290db2
Artur <artur.gawrych@gmail.com>
2025-06-20 22:29:32 +0200

    add main function

--- main.py
+++ main.py
@@ -5,28 +5,33 @@ import sys
 from google.genai import types
 import argparse
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+        
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("user_prompt", help="Specify the prompt.")
-parser.add_argument("--verbose",help="Enable verbosity", action="store_true")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("user_prompt", help="Specify the prompt.")
+    parser.add_argument("--verbose",help="Enable verbosity", action="store_true")
+    args = parser.parse_args()
 
-user_prompt = args.user_prompt
+    user_prompt = args.user_prompt
 
-messages = [
-types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001',
-    contents=messages
-)
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages
+    )
 
-print(response.text)
+    print(response.text)
 
-if args.verbose:
-    print("User prompt:", user_prompt)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if args.verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+if __name__ == "__main__":
+    main()
