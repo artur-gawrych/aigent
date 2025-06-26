f6d937567a8f402d8644cf89a1de0881caefa586
Artur <artur.gawrych@gmail.com>
2025-06-26 08:09:08 +0200

    return error strings, add unittests

--- functions/functions_tests.py
+++ functions/functions_tests.py
@@ -12,6 +12,38 @@ class TestGetFilesInfo(unittest.TestCase):
             "- tests.py: file_size=1342 bytes, is_dir=False"
             ])
         )
+    
+    def test_directory_pkg(self):
+        result = get_files_info("calculator", "pkg")
+        self.assertEqual(
+            result,
+            "\n".join([
+            "- __pycache__: file_size=4096 bytes, is_dir=True",
+            "- calculator.py: file_size=1737 bytes, is_dir=False",
+            "- render.py: file_size=766 bytes, is_dir=False"
+        ])
+        )
+    
+    def test_directory_slash(self):
+        result = get_files_info("calculator", "/bin")
+        self.assertEqual(
+            result,
+            f'Error: Cannot list "/bin" as it is outside the permitted working directory'
+        )
+    def test_directory_traversal(self):
+        result = get_files_info("calculator", "../")
+        self.assertEqual(
+            result,
+            f'Error: Cannot list "../" as it is outside the permitted working directory'
+        )
+
+    def test_directory_invalid(self):
+        result = get_files_info("calculator", "calculator")
+        self.assertEqual(
+            result,
+            f'Error: Cannot list "calculator" as it is outside the permitted working directory'
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,18 +3,15 @@ import sys
 
 def get_files_info(working_directory, directory=None):
     if directory is None or len(directory) == 0:
-        print(f"Error: Please specify the directory.")
-        return
+        return f'Error: Please specify the directory'
     if directory.startswith("../") or directory.startswith("/"):
-        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        return
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     abs_path = os.path.abspath(working_directory)
     joined_paths = os.path.join(abs_path, directory)
 
     if not os.path.isdir(joined_paths):
-        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        return    
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     dir_items = os.listdir(joined_paths)
     dir_info = []
@@ -26,9 +23,9 @@ def get_files_info(working_directory, directory=None):
         item_info = f"- {item}: file_size={file_size} bytes, is_dir={is_dir}"
         dir_info.append(item_info)
     
-    dir_info_sorted = sorted(dir_info)
-    result = "\n".join(dir_info_sorted)
+    result = "\n".join(sorted(dir_info))
+
     print(result)
     return result
 
-get_files_info("calculator", ".")
+get_files_info("calculator", "llll")
