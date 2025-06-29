ea9f77cfdfd9d06decc227b73cc3b5b397d4ef73
Artur <artur.gawrych@gmail.com>
2025-06-29 10:13:06 +0200

    add schema for get_files_info

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info
+        ]
+    )
+
     parser = argparse.ArgumentParser()
     parser.add_argument("user_prompt", help="Specify the prompt.")
     parser.add_argument("--verbose","-v",help="Enable verbosity", action="store_true")
@@ -24,15 +44,29 @@ def main():
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt)
     )
 
-    print(response.text)
+    if response.function_calls:
+        for function in response.function_calls:
+            print(f"Calling function: {function.name}({function.args})")
+    else:
+        print(response.text)
 
     if args.verbose:
         print("User prompt:", user_prompt)
