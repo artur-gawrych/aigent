f19c7d40b83fb3c9c4b39ad88751fb3aae0f0215
Artur <artur.gawrych@gmail.com>
2025-06-29 12:15:56 +0200

    add schema_write_file

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def write_file(working_path, file_path, content):
 
@@ -18,3 +19,37 @@ def write_file(working_path, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f'Error: Unable to write to a file: {e}'
+    
+
+schema_write_file = types.FunctionDeclaration(
+name="write_file",
+description="Write contents to a file. If file deoes not exist it will create one. Constrained to the working directory.",
+parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+        "file_path": types.Schema(
+            type= types.Type.STRING,
+            description="Path to a file, relative to working directory."
+        ),
+        "content": types.Schema(
+            type=types.Type.STRING,
+            description="Content to be written to a file."
+        )
+    }
+
+)
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+name="run_python_file",
+description="Runs python script, constrained to the working directory.",
+parameters=types.Schema(
+    type=types.Type.OBJECT,
+    properties={
+        "file_path": types.Schema(
+            type=types.Type.STRING,
+            description="Path to a python sctipt, relative to working directory"
+        )
+    }
+)
+)
